83f195b73bf6f54da847bc0b2bec59b70ad51b66
Júllya Cabral <124018755+Vct-Jully@users.noreply.github.com>
2025-05-14 15:13:58 -0300

    Update product.py

--- models/product.py
+++ models/product.py
@@ -1,8 +1,20 @@
+from models.observer import Subject
+
 products = []
 discounts = {}
 product_reviews = {}
 
-class Product:
+class Product(Subject):
+    def __init__(self, nome, preco):
+        super().__init__()
+        self.nome = nome
+        self.preco = preco
+
+    @staticmethod
+    def add_product(nome, preco):
+        produto = Product(nome, preco)
+        products.append({"nome": nome, "preco": preco})
+        produto.notificar(f"Novo produto disponível: {nome} por R${preco}!")class Product:
     def __init__(self, nome, preco):
         self.nome = nome
         self.preco = preco
